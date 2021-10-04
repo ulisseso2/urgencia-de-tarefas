c1cd9510fc015c603b17dfd8c24d911668583dd4
Ulisses Oliveira de Oliveira <ulissesrce@gmail.com>
2021-10-04 12:30:35 -0300

    Criar tabela

---
+++ urgencia de tarefas/index.js
@@ -0,0 +1,7 @@
+// Pegar elementos do input
+//pegar ação do click
+// separar as urgências de acordo com a classificação do range
+// criar filhos td das linhas importante e pouco importante
+// Criar ifs 
+
+
